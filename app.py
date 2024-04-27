92a763ab7bbba5ea327464a02b3a83e279839a7f
Savva Bakin <savva2005bakin@gmail.com>
2024-04-27 14:55:08 +0300

    modified app.py, add main.js, modified index.html

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask('__name__')
 
+games = [
+    {'name':'Bloodborne',
+     'year': 2015,
+     'ready':True,
+     'id':0
+    },
+    {'name':'Assassin\'s Creed II',
+     'year': 2009,
+     'ready': False,
+     'id':1
+    },
+    {'name':'Half-Life',
+     'year': 1998,
+     'ready': True,
+     'id':2
+    },
+    {'name':'Doom Eternal',
+     'year': 2020,
+     'ready': False,
+     'id':3
+    }
+]
+
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', games_list=games)
+
+@app.route('/ready/<int:game_id>', methods=['PATCH'])
+def modify(game_id):
+    global games
+    ready = request.json['ready']
+    for game in games:
+        if game['id'] == game_id:
+            game.update({'ready': ready})
+    return 'Ok'
+
+@app.route('/game', methods=['POST'])
+def create_task():
+    data = request.json['']
+    last_id = games[-1]['id']
+    new_id = last_id + 1
+    data['id'] = new_id
+    games.append(data)
+    return 'Ok'
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,17 @@
+function updateGame(el){
+    game_id = el.value
+    fetch('/ready/' + game_id, {
+        method: 'patch', 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'ready': el.checked})
+    })
+}
+function createGame(el){
+    let name = document.getElementById('name').value
+    let year = document.getElementById('year').value
+    fetch('/game', {
+        method: 'post', 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'name': name || 'Пустое', 'year': year || '0000', 'ready': false})
+    })
+}
